8f118a7ec59496436da23d3136ffeb2e9768f2fb
birkti88 <nbirkti@gmail.com>
2024-04-17 00:41:18 -0700

    up.js added

---
+++ up.js
@@ -0,0 +1,8 @@
+console.log("hello.world");
+let firstName="Birkti";
+firstName="Birkti";
+let age="28";
+let address="Addis Ababa";
+let isStudent="true";
+console.log(firstName);
+console.log(`my name is: ${firstName} my address is: ${address} are you a student: ${isStudent}`);
